eed6a094daf314f2b44c57fbb4c46cff7de432c9
Evelyn Vasconcelos <enixiegamer@gmail.com>
2024-06-22 13:20:48 -0300

    Style: definindo estrutura e estilo

---
+++ JS/script.js
@@ -0,0 +1,18 @@
+// SIDEBAR
+
+var sidebarOpen = false;
+var sidebar = document.getElementById("sidebar");
+
+function openSidebar() {
+    if(!sidebarOpen) {
+        sidebar.classList.add("sidebar-responsive");
+        sidebarOpen = true;
+    } // OPEMSIDEBAR
+}
+
+function closeSidebar() {
+    if(sidebarOpen) {
+        sidebar.classList.remove("sidebar-responsive");
+        sidebarOpen = false;
+    }
+}
