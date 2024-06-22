6b435e69ba5567af70dd08162d101e114a028b5c
Evelyn Vasconcelos <enixiegamer@gmail.com>
2024-06-22 20:21:09 -0300

    Feat: media queries

--- JS/script.js
+++ JS/script.js
@@ -146,3 +146,120 @@ var barChartOptions = {
 
   var barChart = new ApexCharts(document.querySelector("#bar-chart"), barChartOptions);
   barChart.render();
+
+// AREA CHART
+
+var areaChartOptions = {
+    series: [{
+        name: 'Mês anterior',
+        data: [44, 55, 31, 47, 31, 43, 26, 41, 31, 47, 33, 44]
+    }, {
+        name: 'Mês atual',
+        data: [55, 69, 45, 61, 43, 54, 37, 52, 44, 61, 43, 52]
+    }],
+    chart: {
+        type: "area",
+        background: "transparent",
+        height: 350,
+        stacked: false,
+        toolbar: {
+            show: false,
+        },
+    },
+    colors: ["#00ab57", "#d50000"],
+    labels: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "De"],
+    dataLabels: {
+        enabled: false,
+    },
+    fill: {
+        gradient: {
+            opacityFrom: 0.4,
+            opacityTo: 0.1,
+            shadeIntensity: 1,
+            stops: [0, 100],
+            type: "vertical",
+        },
+        type: "gradient",
+    },
+    grid: {
+        borderColor: "#55596e",
+        yaxis: {
+            lines: {
+                show: true,
+            },
+        },
+        xaxis: {
+            lines: {
+                show: true,
+            },
+        },
+    },
+    legend: {
+        labels: {
+            colors: "#f5f7ff",
+        },
+        show: true,
+        position: "top",
+    },
+    markers: {
+        size: 6,
+        strokeColors: "#1b2635",
+        strokeWidth: 3
+    },
+    stroke: {
+        curve: "smooth",
+    },
+    xaxis: {
+        axisBorder: {
+            color: "#55596e",
+            show: true,
+        },
+        axisTicks: {
+            color: "#55596e",
+            show: true,
+        },
+        labels: {
+            offsetY: 5,
+            style: {
+                colors: "#f5f7ff"
+            },
+        },
+    },
+    yaxis: [
+        {
+            title: {
+                text: 'Mês anterior',
+                style: {
+                    color: "#f5f7ff",
+                },
+            },
+            labels: {
+                style: {
+                    colors: ["#f5f7ff"],
+                },
+            },
+        },
+        {
+            opposite: true,
+            title: {
+                text: 'Mês atual',
+                style: {
+                    color: "#f5f7ff"
+                },
+            },
+            labels: {
+                style: {
+                    colors: ["#f5f7ff"],
+                },
+            },
+        },
+    ],
+    tooltip: {
+        shared: true,
+        intersect: false,
+        theme: "dark",
+    }
+};
+
+var areachart = new ApexCharts(document.querySelector("#area-chart"), areaChartOptions);
+areachart.render();
